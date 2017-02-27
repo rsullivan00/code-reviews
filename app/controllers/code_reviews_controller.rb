1daac9a284afd57957cb2672c7738106f06fafcc
Rick Sullivan <rsullivan@focusvision.com>
2017-02-26 20:03:14 -0800

    Handle button action

--- app/controllers/code_reviews_controller.rb
+++ app/controllers/code_reviews_controller.rb
@@ -1,7 +1,6 @@
 class CodeReviewsController < ActionController::Base
-  before_action :validate_slack_token
-
   def index
+    validate_slack_token(params['token'])
     GetCodeReviews.perform_async(response_url: params[:response_url])
 
     render(
@@ -14,15 +13,33 @@ class CodeReviewsController < ActionController::Base
   end
 
   def actions
+    payload = JSON.parse(params['payload'])
+    validate_slack_token(payload['token'])
+
+    original_message = payload['original_message']
+    original_message['attackments'] = original_message['attachments'].map do |attachment|
+      if attachment['callback_id'] == payload['callback_id']
+        attachment['text'] = "#{attachment['text']}@#{payload['user']['name']} "
+      end
+      attachment
+    end
+
+    render(
+      json: original_message.to_json,
+      status: 200
+    )
   end
 
   private
 
-  def validate_slack_token
-    invalid_slack_token! unless params['token'] == ENV['SLACK_TOKEN']
+  def validate_slack_token(token)
+    invalid_slack_token! unless token == ENV['SLACK_TOKEN']
   end
 
   def invalid_slack_token!
-    render json: { errors: ['Slack token invalid']}
+    render json: {
+      response_type: 'ephemeral',
+      text: 'The Slack token sent to the server is invalid'
+    }
   end
 end
